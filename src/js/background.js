7da95dca0141ccf8a37ab72056fa978fef3ecf60
Bruce Schwartz <bruce@z2a.org>
2020-01-23 10:31:51 -0800

    added user_map and auth_map local storage

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,4 @@
+"use strict"
 /* background.js
  *
  * This file has an example of how to make variables accessible to other scripts of the extension.
@@ -7,18 +8,174 @@
  * Note that not all extensions need of a background.js file, but extensions that need to persist data after a popup has closed may need of it.
  */
 
-// A sample object that will be exposed further down and used on popup.js
-const sampleBackgroundGlobal = {
-    message: 'This object comes from background.js'
-};
+var user_map 
+var auth_map 
+var tabDict = {}
+
+function now() {
+    return Math.floor((new Date()).getTime() / 1000)
+}
+
+chrome.storage.local.get('user_map', function(map) {
+    user_map = (map && map.user_map) || {}
+    console.log('user_map:', JSON.stringify(user_map, null, 4))
+})
+
+chrome.storage.local.get('auth_map', function(map) {
+    auth_map = (map && map.auth_map) || {}
+    console.log('auth_map:', JSON.stringify(auth_map, null, 4))
+    checkAuths()
+})
+
+function setUser(id_str, name, screen_name) {
+    user_map[id_str] = { name: name, screen_name: screen_name }
+    userMapChanged()
+}
+
+function setAuth(auth, csrf) {
+    var data = auth_map[auth]
+    if (data && data.csrf === csrf) return
+    data = data || {}
+    data.timestamp = now()
+    data.csrf = csrf
+    data.update = true  // new csrf, needs update
+    auth_map[auth] = data
+    authMapChanged()
+}
+
+function authMapChanged() {
+    console.log('saving auth_map')
+    chrome.storage.local.set({ auth_map: auth_map }, function() {
+        console.log('...saved auth_map')
+    })
+}
+
+function userMapChanged() {
+    console.log('saving user_map')
+    chrome.storage.local.set({ user_map: user_map }, function() {
+        console.log('...saved user_map')
+    })
+}
+
+function checkAuths() {
+    for (var auth in auth_map) {
+        checkAuth(auth)
+    }
+}
+
+function checkAuth(auth) {
+    var data = auth_map[auth]
+    if (!data) return
+    if (!data.csrf) return
+
+    if (data.id_str && !user_map[data.id_str]) {
+        setUser(data.id_str, data.name, data.screen_name)
+    }
+
+    if (data.fetched && !data.update) return
+    console.log('checking', auth)
+    const url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+    fetch(url, { headers: {
+        'authorization': auth,
+        'x-csrf-token': data.csrf
+    }})
+    .then(function(response) {
+        response.text().then(function(json) {
+            var obj = JSON.parse(json)
+            console.log('checkAuth response: ', JSON.stringify(obj, null, 4))
+            data.screen_name = obj.screen_name
+            data.id_str = obj.id_str
+            data.name = obj.name
+            data.fetched = true
+            auth_map[auth] = data
+            authMapChanged()
+            setUser(obj.id_str, { name: obj.name, screen_name: obj.screen_name} )
+        })
+    })
+    .catch(function(error) {
+        console.log('checkAuth failed:', error.message)
+        data.failures = data.failures || 0
+        data.failures += 1
+        auth_map[auth] = data
+        authMapChanged()
+    })
+
+}
 
 // Listen to short lived messages from in-content.js
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     // Perform any ther actions depending on the message
-    console.log('background.js - received message from in-content.js:', message);
     // Respond message
-    sendResponse('👍');
-});
+    var tabId = sender.tab.id
+    if (!tabDict[tabId]) {
+        console.log('background, no response for tabId: ' +  tabId)
+        sendResponse(null)
+        return
+    }
 
-// Make variables accessible from chrome.extension.getBackgroundPage()
-window.sampleBackgroundGlobal = sampleBackgroundGlobal;
+    console.log('background, send response to tabId: ' +  tabId, tabDict[tabId])
+    sendResponse(tabDict[tabId])
+})
+
+
+// Intercept all requests to twitter.com to look for the
+// authorization header
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+        if (details.method == 'OPTIONS') return
+        console.log('webRequest.onBeforeSendHeaders **** url: ' + details.url)
+        //console.log('webRequest.onBeforeSendHeaders details: ' + JSON.stringify(details, null, 4))
+        var tabId = details.tabId
+        if (tabId === -1) return
+        var data = { }
+        for (var header of details.requestHeaders) {
+            if (header.name == 'authorization') {
+                data.auth = header.value
+            }
+            if (header.name == 'x-csrf-token') {
+                data.csrf = header.value
+            }
+        }
+        if (data.auth && data.csrf) {
+
+            setAuth(data.auth, data.csrf)
+
+            data = Object.assign(data, {
+                tabId: tabId,
+                timeStamp: now(),
+                url: details.url,
+            })
+            tabDict[tabId] = data
+            //console.log('tab dict:', JSON.stringify(tabDict, null, 4))
+        }
+    },
+    {urls: ['https://api.twitter.com/*']},
+    [ 'requestHeaders' ]
+)
+
+function try_it() {
+    var url
+    url = 'https://api.twitter.com/1.1/friends/list.json?skip_status=1&user_id=3010607233'
+    url = 'https://api.twitter.com/1.1/friends/list.json?skip_status=1'
+    url = 'https://api.twitter.com/1.1/friends/ids.json'
+    url = 'https://api.twitter.com/2/badge_count/badge_count.json?supports_ntab_urt=1'
+    url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+    fetch(url, { headers: {
+        'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+        //'x-csrf-token': '2c4fa9d3112665efdef6a496bfc142de'
+        //'x-csrf-token': '11d1c6ae07b4599a6e0263ef19d6ba5d'
+        //'x-csrf-token': '0082370c868eb5f5f466ed8c144aecc1'
+        'x-csrf-token': '0082370c868eb5f5f466ed8c144aecc1'
+    }})
+    .then(function(response) {
+        console.log('response status:', response.status)
+        response.text().then(function(data) {
+            //console.log(JSON.stringify(JSON.parse(data), null, 4))
+            console.log(JSON.stringify(JSON.parse(data)))
+        })
+    })
+    .catch(function(error) {
+        console.log('failed to load:', error.message)
+    })
+}
+//try_it()
+console.log('loaded background')
--- src/js/in-content.js
+++ src/js/in-content.js
@@ -29,8 +29,60 @@ chrome.extension.onConnect.addListener(popupPort => {
 });
 
 // Response handler for short lived messages
-const handleBackgroundResponse = response =>
-    console.log('in-content.js - Received response:', response);
+function  handleBackgroundResponse(response) {
+    console.log('in-content - Received response:', JSON.stringify(response))
+    if (response) {
+        do_it(response.auth, response.csrf)
+        return
+    }
+    console.log('waiting for auth')
+    setTimeout(function() {
+        chrome.runtime.sendMessage('in-content', handleBackgroundResponse)
+    }, 250)
+}
+//chrome.runtime.sendMessage('in-content', handleBackgroundResponse)
 
-// Send a message to background.js
-chrome.runtime.sendMessage('Message from in-content.js!', handleBackgroundResponse);
+function do_it(auth, csrf) {
+    
+    console.log('following: try_it()')
+    var url
+    url = 'https://api.twitter.com/1.1/friends/following/list.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cursor=-1&user_id=3010607233&count=3&with_total_count=true'
+    url = 'https://api.twitter.com/1.1/friends/following/list.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cursor=-1&user_id=3010607233&count=3&with_total_count=true'
+    url = 'https://api.twitter.com/1.1/followers/list.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cursor=-1&user_id=3010607233&count=20'
+    url = 'https://api.twitter.com/1.1/friends/list.json'
+
+    if (true) {
+        fetch(url, {
+            credentials: 'include',
+            headers: {
+                'authorization': auth,
+                'x-csrf-token': csrf,
+                'x-twitter-active-user': 'yes',
+                'x-twitter-auth-type': 'OAuth2Session',
+            }
+        })
+        .then(function(response) {
+            console.log('response status:', response.status)
+            response.text().then(function(data) {
+                console.log(data)
+            })
+
+        })
+        .catch(function(error) {
+            console.log('failed to load:', error.message)
+        })
+    }
+    else {
+        req.onload = function(e) {
+            console.log('event:', e)
+            console.log('response:', e.response)
+        }
+        req.open("GET", url)
+        //req.setRequestHeader('origin', 'https://twitter.com')
+        req.setRequestHeader('authorization', auth)
+        req.setRequestHeader('x-csrf-token', '2c4fa9d3112665efdef6a496bfc142de')
+        req.setRequestHeader('x-twitter-active-user', 'yes')
+        req.setRequestHeader('x-twitter-auth-type', 'OAuth2Session')
+        req.send()
+    }
+}
